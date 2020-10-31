cffbd75de7cabd6a100a8f411069cdac66ee4831
Nuraini <nurroplet45@gmail.com>
2020-10-31 15:23:03 +0700

    Add project files.

---
+++ ServerdanClient_Nuraini__072/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerdanClient_Nuraini__072
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = true;
+            label3.Text = "Klik OFF Untuk Mematikan Server";
+            label2.Text = "Server ON";
+
+            Server server = new Server();
+            server.OnServer();
+        }
+    }
+}
